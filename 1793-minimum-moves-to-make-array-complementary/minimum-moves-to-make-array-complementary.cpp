6973d0e98a14ee90c643aaea502e150fae856606
Chereches Tudor <137445044+chereches-tudor-30126@users.noreply.github.com>
2026-05-13 22:52:10 +0300

    Time: 3 ms (88.07%) | Memory: 85.9 MB (94.50%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-    int minMoves(vector<int>& nums, int limit) {
-        int n = nums.size();
-       
-        vector<int> delta(2 * limit + 2, 0);
+    int minMoves(vector<int>& nums, int limit) {  
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         
-        for (int i = 0; i < n / 2; ++i) {
-            int A = min(nums[i], nums[n - 1 - i]);
-            int B = max(nums[i], nums[n - 1 - i]);
+        int n = nums.size();
+        
+        int delta[200005] = {0}; 
+        
+        int half = n / 2;
+        int* p_nums = nums.data(); 
+        
+        for (int i = 0; i < half; ++i) {
+            int a = p_nums[i];
+            int b = p_nums[n - 1 - i];
+
+            if (a > b) {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
             
-           
-            delta[1 + A] -= 1;             
-            delta[A + B] -= 1;            
-            delta[A + B + 1] += 1;       
-            delta[B + limit + 1] += 1;     
+            delta[1 + a] -= 1;
+            delta[a + b] -= 1;
+            delta[a + b + 1] += 1;
+            delta[limit + b + 1] += 1;
         }
         
-      
-        int current_moves = n; 
+        int current_moves = n;
         int min_moves = n;
+        int max_target = 2 * limit;
         
-       
-        for (int i = 2; i <= 2 * limit; ++i) {
+        for (int i = 2; i <= max_target; ++i) {
             current_moves += delta[i];
-            min_moves = min(min_moves, current_moves);
+            if (current_moves < min_moves) {
+                min_moves = current_moves;
+            }
         }
         
         return min_moves;
