f8afda8ef214036b45c4f0defe38c6fa9fcfb105
Chereches Tudor <137445044+chereches-tudor-30126@users.noreply.github.com>
2026-08-01 11:59:31 +0300

    Time: 0 ms (100.00%) | Memory: 10.3 MB (9.53%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int l=0, r=matrix.size()-1;
+
+        while(l<r){
+            for(int i=0;i<r-l;i++){
+                int aux=matrix[l][l+i];
+                matrix[l][l+i]=matrix[r-i][l];
+                matrix[r-i][l]=matrix[r][r-i];
+                matrix[r][r-i]=matrix[l+i][r];
+                matrix[l+i][r]=aux;
+            }
+            l++;
+            r--;
+
+        }
+                
+    }
+};
